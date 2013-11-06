1549117d6e089d319751945dfd371a50e5e9e51e
Oliver Weyhmueller <oliver.weyhmueller@t-systems.com>
2013-11-06 08:41:18 +0100

    Add SSL Support in jetty (not working yet)

--- ext/jetty/src/main/java/JettyWarMain.java
+++ ext/jetty/src/main/java/JettyWarMain.java
@@ -1,6 +1,13 @@
 
 import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -27,9 +34,32 @@ public class JettyWarMain {
         webapp.setDefaultsDescriptor(webdefaultPath());
 
         Server server = new Server();
-        Connector connector = new SelectChannelConnector();
-        connector.setPort(Integer.getInteger("jetty.port",8080).intValue());
-        server.setConnectors(new Connector[]{connector});
+
+        HttpConfiguration http_config = new HttpConfiguration();
+        http_config.setSecureScheme("https");
+        http_config.setSecurePort(8443);
+        http_config.setOutputBufferSize(32768);
+        ServerConnector http = new ServerConnector(server,new HttpConnectionFactory(http_config));
+        http.setPort(8080);
+        http.setIdleTimeout(30000);
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(System.getProperty("jetty.home") + "/etc/keystore");
+        sslContextFactory.setKeyStorePassword("OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4");
+        sslContextFactory.setKeyManagerPassword("OBF:1u2u1wml1z7s1z7a1wnl1u2g");
+        HttpConfiguration https_config = new HttpConfiguration(http_config);
+        https_config.addCustomizer(new SecureRequestCustomizer());
+
+        ServerConnector https = new ServerConnector(server,
+            new SslConnectionFactory(sslContextFactory,"http/1.1"),
+            new HttpConnectionFactory(https_config));
+        https.setPort(8443);
+        https.setIdleTimeout(500000);
+
+
+        //Connector connector = new SelectChannelConnector();
+        //connector.setPort(Integer.getInteger("jetty.port",8080).intValue());
+        //server.setConnectors(new Connector[]{connector});
+        server.setConnectors(new Connector[] { http, https });
         server.setHandler(webapp);
         server.start();
         server.join();
